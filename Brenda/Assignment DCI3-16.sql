bd4444cc7a557fa058bcd1c428cc9c348632ba5f
Brenda Aguero Carranza <baguero.ba@gmail.com>
2022-08-27 19:39:59 -0500

    updated assignment

--- Brenda/Assignment DCI3-16.sql
+++ Brenda/Assignment DCI3-16.sql
@@ -5,11 +5,5 @@ select stor_id FROM stores
 select stor_id FROM sales
 
 -- find max quanity in sales table from store id 71731--
-SELECT count(S.stor_id) FROM sales SA
-INNER JOIN stores S on SA.stor_id = S.stor_id
-WHERE S.stor_id = '7131'
-
--- use this to double check there is indeed 6 of the quantity in the sales table for the stor_id 7131--
-
-select stor_id FROM sales 
-where stor_id = '7131'
+SELECT MAX(qty) FROM sales
+WHERE stor_id = '7131'
